fe8d74a4e69dff4a1857030e7cc9475367a685e1
Георги Тучев <g.tuchev@gmail.com>
2022-04-17 23:56:08 +0300

    Initial commit

--- employees_app/settings.py
+++ employees_app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 from os.path import join
 from pathlib import Path
 
@@ -76,10 +77,17 @@ WSGI_APPLICATION = 'employees_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
     }
 }
 
